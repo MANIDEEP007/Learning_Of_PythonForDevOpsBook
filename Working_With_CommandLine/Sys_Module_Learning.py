d40d4f8c7ae25a7e6a016aee63b46fa1ed0b7a49
Paduchuri Manideep <paduchurimanideep@gmail.com>
2021-05-22 20:14:44 +0530

    Initial Sys Module learning

---
+++ Working_With_CommandLine/Sys_Module_Learning.py
@@ -0,0 +1,12 @@
+#Author : Manideep Paduchuri
+
+'''
+About sys module
+----------------------
+An Python built-in module
+Module for interacting with system & shell
+'''
+
+import sys
+print(sys.byteorder) #Prints byte order of the architecture
+print(sys.platform) #Prints Platform of the system in use
