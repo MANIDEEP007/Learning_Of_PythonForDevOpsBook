b8e409edf15fab2230af0eace811fcce5038f65a
Paduchuri Manideep <paduchurimanideep@gmail.com>
2021-06-12 18:44:23 +0530

    Fix Comment

--- Working_With_CommandLine/Argument_Parser.py
+++ Working_With_CommandLine/Argument_Parser.py
@@ -17,7 +17,7 @@ arg_paser = argparse.ArgumentParser(description='Demo for Learning')
 arg_paser.add_argument('First_Num',help="Input First Number")
 arg_paser.add_argument('Second_Num',help="Input Second Number")
 
-#Arguments starting with -- means its optional
+#Arguments starting with -- means its optional and should use short / long hand option to pass value
 arg_paser.add_argument('--Third_Num','-t',help="Provide third number if needed")
 
 #Parse the Arguments - Fails if mandate arguments are not provided
